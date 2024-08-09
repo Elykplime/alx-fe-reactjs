be9b3dc636f860ba500ffba961567a58ca0bb2b6
Elykplime <akakpoyvettee@gmail.com>
2024-08-09 17:07:16 +0000

    welcome message modified

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,13 +1,12 @@
-import WelcomeMessage from './components/WelcomeMessage';
+import React from 'react';
 
 function WelcomeMessage() {
-    return (
-        <div>
-            <h1>Hello everyone, I am learning React at ALX!</h1>
-            <p>This is a simple JSX component.</p>
-            <p>I am learning about JSX!</p>
-        </div>
-    );
+  return (
+    <>
+      <h1>Hello everyone, I am learning React at ALX!</h1>
+      <p>I am learning about JSX!</p>
+    </>
+  );
 }
 
 export default WelcomeMessage;
