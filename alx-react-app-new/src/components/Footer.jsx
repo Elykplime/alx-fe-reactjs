1ceb592eac3cd9d2412ee36adaeb88bf646598b7
Elykplime <akakpoyvettee@gmail.com>
2024-08-17 20:33:51 +0000

    Files modified and inline styling to react components added

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
-        <footer>
-            <p>© 2023 City Lovers</p>
+        <footer style={{ backgroundColor: '#009688'}}>
+            <p  style={{ color: 'white', textAlign: 'center' }}>© 2023 City Lovers</p>
         </footer>
     );
 }
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
     return (
-        <main>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
+        <main style={{ backgroundColor: '#eee'}}>
+            <p style={{ color: 'black', textAlign: 'center' }}>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
 }
